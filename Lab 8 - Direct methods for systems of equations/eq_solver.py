8538dd2e2596d696b3143888a20c61a33bb5f51f
Devedir <109344310+Devedir@users.noreply.github.com>
2024-05-07 01:03:25 +0200

    LU decomposition

--- Lab 8 - Direct methods for systems of equations/eq_solver.py
+++ Lab 8 - Direct methods for systems of equations/eq_solver.py
@@ -1,15 +1,56 @@
+import numpy as np
+import time
+
+
 def main(n: int) -> (int, int, int):
-    # generate A(n x n) matrix
-    # generate b(n) vector
+    A = np.random.uniform(-10., 10., (n, n))
+    A[A == 0] = 0.0001  # for Doolittle's method to work
+    B = np.random.uniform(-10, 10, n)
+
+    expected = np.linalg.solve(A, B)
+
     # for each method:
     #     calculate result
     #     check for correctness
     #     measure time
-    return 1, 2, 3  # measured times
+
+    # LU decomposition
+    lu_time = time.time()
+
+    L = np.eye(n, dtype=np.float64)
+    U = np.zeros((n, n), dtype=np.float64)
+
+    U[0, 0] = A[0, 0]
+    for i in range(n):
+        for j in range(i, n):
+            U[i, j] = A[i, j] - np.dot(L[i, :i], U[:i, j])
+        for j in range(i+1, n):
+            L[j, i] = (A[j, i] - np.dot(L[j, :i], U[:i, i])) / U[i, i]
+
+    z = np.linalg.solve(L, B)
+    x = np.linalg.solve(U, z)
+
+    lu_time = time.time() - lu_time
+
+    if not np.allclose(x, expected):
+        raise AssertionError("LU decomposition got it wrong!")
+
+    # inversion
+    inv_time = time.time()
+    # some stuff
+    inv_time = time.time() - inv_time
+
+    # QR decomposition
+    qr_time = time.time()
+    # some stuff
+    qr_time = time.time() - qr_time
+
+    return lu_time, inv_time, qr_time
 
 
 if __name__ == '__main__':
-    LU_time, inversion_time, QR_time = main(5)
+    # TODO: argument user input
+    LU_time, inversion_time, QR_time = main(4)
     print('For equation size = 5:')
     print(f'{LU_time = }')
     print(f'{inversion_time = }')
